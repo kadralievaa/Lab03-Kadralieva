cb1cb71b5f080ebd3279bb361c65daedeb269116
Кадралиева Аделина <adel.kadralieva@gmail.com>
2026-09-15 23:06:59 +0300

    Lab03 task 2: increment in expressions

--- Program.cs
+++ Program.cs
@@ -132,14 +132,36 @@
 
 
 //
+// Console.WriteLine();
+// Console.WriteLine("Чётное или нечётное");
+
+// Console.Write("Введите целое число: ");
+// int number = int.Parse(Console.ReadLine());
+// bool isEven = (number % 2 == 0);
+// Console.WriteLine($"Число чётное: {isEven}");
+
+
 Console.WriteLine();
-Console.WriteLine("Чётное или нечётное");
+Console.WriteLine("Инкремент в выражении");
 
-Console.Write("Введите целое число: ");
-int number = int.Parse(Console.ReadLine());
-bool isEven = (number % 2 == 0);
-Console.WriteLine($"Число чётное: {isEven}");
+// постфикс сначала отдаёт старое значение, потом увеличивает
+int a = 5;
+int b = a++;
+// a стало 6, b получило старое значение 5
+Console.WriteLine($"a = 5; b = a++;   a = {a}, b = {b}");
 
+//префикс сначала увеличивает, потом отдаёт новое значение
+int c = 5;
+int d = ++c;
+// c стало 6, d получило уже новое значение 6
+Console.WriteLine($"c = 5; d = ++c;   c = {c}, d = {d}");
+
+// смешанное в одном выражении
+int x = 10;
+int y = x++ + ++x;
+// x++ отдаёт 10 (x становится 11), ++x увеличивает до 12 и отдаёт 12
+// получается, y = 10 + 12 = 22, x = 12
+Console.WriteLine($"x = 10; y = x++ + ++x;   x = {x}, y = {y}");
 
 
 
