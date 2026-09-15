3f6ac202cd41741c965cf1486a02c78121cfbc20
Кадралиева Аделина <adel.kadralieva@gmail.com>
2026-09-15 22:31:25 +0300

    Lab03 task 1: even or odd check

--- Program.cs
+++ Program.cs
@@ -102,33 +102,45 @@
 
 
 //19
+// Console.WriteLine();
+// Console.WriteLine("Приёмная комиссия");
+
+// Console.Write("Введите средний балл аттестата: ");
+// double averageGrade = double.Parse(Console.ReadLine());
+
+// Console.Write("Введите баллы за экзамен (0-100): ");
+// int examScore = int.Parse(Console.ReadLine());
+
+// Console.Write("Есть льгота? (1 - да, 0 - нет): ");
+// int benefitInput = int.Parse(Console.ReadLine());
+// bool hasBenefit = (benefitInput == 1);
+
+// bool hasGoodCertificate = averageGrade >= 4.0;
+// bool hasGoodExam = examScore >= 60;
+// bool isEligibleByRules = (hasGoodCertificate && hasGoodExam) || hasBenefit;
+
+// double totalScore = averageGrade * 10;
+// totalScore += examScore;
+
+// Console.WriteLine();
+// Console.WriteLine("Результат");
+// Console.WriteLine($"Хороший аттестат (>= 4.0): {hasGoodCertificate}");
+// Console.WriteLine($"Хороший экзамен (>= 60): {hasGoodExam}");
+// Console.WriteLine($"Льгота: {hasBenefit}");
+// Console.WriteLine($"Проходит по правилам: {isEligibleByRules}");
+// Console.WriteLine($"Итоговый балл: {totalScore}"); 
+
+
+//
 Console.WriteLine();
-Console.WriteLine("Приёмная комиссия");
+Console.WriteLine("Чётное или нечётное");
 
-Console.Write("Введите средний балл аттестата: ");
-double averageGrade = double.Parse(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+bool isEven = (number % 2 == 0);
+Console.WriteLine($"Число чётное: {isEven}");
 
-Console.Write("Введите баллы за экзамен (0-100): ");
-int examScore = int.Parse(Console.ReadLine());
 
-Console.Write("Есть льгота? (1 - да, 0 - нет): ");
-int benefitInput = int.Parse(Console.ReadLine());
-bool hasBenefit = (benefitInput == 1);
-
-bool hasGoodCertificate = averageGrade >= 4.0;
-bool hasGoodExam = examScore >= 60;
-bool isEligibleByRules = (hasGoodCertificate && hasGoodExam) || hasBenefit;
-
-double totalScore = averageGrade * 10;
-totalScore += examScore;
-
-Console.WriteLine();
-Console.WriteLine("Результат");
-Console.WriteLine($"Хороший аттестат (>= 4.0): {hasGoodCertificate}");
-Console.WriteLine($"Хороший экзамен (>= 60): {hasGoodExam}");
-Console.WriteLine($"Льгота: {hasBenefit}");
-Console.WriteLine($"Проходит по правилам: {isEligibleByRules}");
-Console.WriteLine($"Итоговый балл: {totalScore}"); 
 
 
 
